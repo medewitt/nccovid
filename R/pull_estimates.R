6358e3ed3b64e06e47f7d69e5e86f50ab871c00f
Michael DeWitt <michael.dewitt@conehealth.com>
2020-11-03 14:08:18 -0500

    update to pull R and cases

---
+++ R/pull_estimates.R
@@ -0,0 +1,51 @@
+#' Pull Latest Estimates
+#' 
+#' All estimates for North Carolina and some select regions have been
+#' precalculated and available for download. These estimates are utilising
+#' the EpiNow2 package
+#' 
+#' @param region a string or string vector, the region or county of interest
+#' @param variable a string or string vector, the measure of interest
+#' @export
+
+pull_estimates <- function(region= NULL, variable ="R"){
+	
+	raw_dat <- data.table::fread("https://raw.githubusercontent.com/medewitt/refresh-restimates/master/output/latest_r_coviddata.csv",
+															 colClasses = c(
+															 	"character",
+															 	"Date",
+															 	"Date",
+															 	"character",
+															 	"character",
+															 	"character",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric",
+															 	"numeric"
+															 ))
+	
+	# Define Targets for return to client
+	# If no region specified, return all.
+	# If no variable define, return all
+	if(!is.null(variable)){
+		variables_available <- match.arg(variable, names(unique(raw_dat$variable)), several.ok = TRUE)
+	}
+	
+	if(is.null(region) && is.null(variable)){
+		return(raw_dat)
+	} else if (is.null(region)){
+		clean_dat <- raw_dat[variable==variables_available]
+		return(clean_dat)
+	} else {
+		clean_dat <- raw_dat[variable %chin% variables_available & county %chin% region]
+		
+		return(clean_dat)
+	}
+	
+	
+}
