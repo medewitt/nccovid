1d98acf644de4822e26639b68fa3799418808695
Michael DeWitt <michael.dewitt@conehealth.com>
2020-11-19 16:05:37 -0500

    updating some functions to pull hospitalisations

--- NEWS.md
+++ NEWS.md
@@ -3,7 +3,8 @@
 * Added logic to account for data dumps from North Carolina with an optional argument in  `get_covid_state` called `report_adj` with a default of `FALSE` to account for two points
   * 2020-09-25 when all antigen tests were added 
   * 2020-11-13/2020-11-14 when data collection times changed and only 10 hours of data were collect.
-
+* Added `get_hospitalizations` to retrieve hospitalisation data 
+* Added North Carolina Healthcare Preparation Coalitions as a data set 
 The current method just imputes the previous days value for September for the death and case incidence on 25 September. 
 For the November reporting change the 13/14 days are averaged across the two days. 
 This should smooth out any reporting irregularity. 
---
+++ R/get_hospitalizations.R
@@ -0,0 +1,24 @@
+#' Get Reported Hospitalisation Data
+#' 
+#' Retrieve Latest Hospitalisation data from NC DHHS. This is not an automatic
+#' Scrap so there could be some delay.
+#' @export
+get_hospitalizations <- function(){
+	dat <- data.table::fread("https://raw.githubusercontent.com/conedatascience/covid-data/master/data/timeseries/hospitalisations.csv")
+	
+	names(dat)<- c("date", "coalition", "region_or_state", "adult_icu_empty", 
+								 "adult_icu_ful", "adult_intensive_care_unit_staffed_bed_capacity", 
+								 "at_admission_confirmed_covid19_patients_in_24_hrs", "at_admission_suspected_covid19_patients_in_24_hrs", 
+								 "full_adult_icu_covid19_positive_patients", "hospital_cnt", "hospital_countby_region", 
+								 "icu_licensed_beds", "inpatient_empty_beds_all_types", "inpatient_full_all_bed_types", 
+								 "licensed_inpatient_beds", "number_of_additional_surge_beds_being_planned", 
+								 "number_of_covid_19_positive_patients_in_hospital", "total_staffed_inpatient_capacity_all_bed_types", 
+								 "ventilator_available", "ventilator_used_not_specific_to_covid_19"
+	)
+	
+	dat$date <- as.Date(dat$date, "%m/%d/%Y")
+	
+	last_date <- max(dat$date)
+	message(sprintf("Data valid as of: %s\nUse with caution.", as.character(last_date)))
+	dat
+}
