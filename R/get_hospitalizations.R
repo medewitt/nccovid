aa90272e167e20a8614fd6aee98af486256f6f2f
Michael DeWitt <michael.dewitt@conehealth.com>
2021-01-08 10:46:27 -0500

    updating for new ed metrics

--- R/get_hospitalizations.R
+++ R/get_hospitalizations.R
@@ -6,13 +6,24 @@
 get_hospitalizations <- function(){
 	dat <- data.table::fread("https://raw.githubusercontent.com/conedatascience/covid-data/master/data/timeseries/hospitalisations.csv")
 	
-	names(dat)<- c("date", "region_or_state", "coalition", "adult_icu_empty", 
-								 "adult_icu_full", "adult_intensive_care_unit_staffed_bed_capacity", 
-								 "at_admission_confirmed_covid19_patients_in_24_hrs", "at_admission_suspected_covid19_patients_in_24_hrs", 
-								 "full_adult_icu_covid19_positive_patients", "icu_licensed_beds", 
-								 "inpatient_empty_beds_all_types", "inpatient_full_all_bed_types", 
-								 "licensed_inpatient_beds", "number_of_covid_19_positive_patients_in_hospital", 
-								 "total_staffed_inpatient_capacity_all_bed_types", "ventilator_available", 
+	names(dat)<- c("date", 
+								 "region_or_state", 
+								 "coalition", 
+								 "adult_icu_empty", 
+								 "adult_icu_full", 
+								 "adult_intensive_care_unit_staffed_bed_capacity", 
+								 "at_admission_confirmed_covid19_patients_in_24_hrs", 
+								 "at_admission_suspected_covid19_patients_in_24_hrs", 
+								 "ed_covid_visits_previous_day" ,
+								 "ed_total_visits_previous_day" ,
+								 "full_adult_icu_covid19_positive_patients", 
+								 "icu_licensed_beds", 
+								 "inpatient_empty_beds_all_types", 
+								 "inpatient_full_all_bed_types", 
+								 "licensed_inpatient_beds", 
+								 "number_of_covid_19_positive_patients_in_hospital", 
+								 "total_staffed_inpatient_capacity_all_bed_types", 
+								 "ventilator_available", 
 								 "ventilator_used_not_specific_to_covid_19")
 	
 	dat$date <- as.Date(dat$date, "%m/%d/%Y")
