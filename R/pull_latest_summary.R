1ae133361049641a6e462864cdb53d2815c3b766
Michael DeWitt <michael.dewitt@conehealth.com>
2021-03-27 16:47:48 -0400

    adding information

--- R/pull_latest_summary.R
+++ R/pull_latest_summary.R
@@ -8,6 +8,13 @@ pull_covid_summary <- function(){
 	
 	dat <- data.table::fread("https://raw.githubusercontent.com/conedatascience/covid-data/master/data/timeseries/nc-summary-stats.csv")
 	
+	dat_death = get_covid_state(reporting_adj = TRUE)[,list(daily_deaths = sum(deaths_daily)), by = "date"]
+	
+	data.table::setnames(dat, old = "daily_deaths", "daily_deaths_reported")
+	
+	dat = merge(dat, dat_death, by = "date")
+	
 	return(dat)
 	
 }
+
