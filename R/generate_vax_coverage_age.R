1f9dc4fc8bb3ebbb02c6b2565e27d7df979b71da
Michael DeWitt <conehealth-ea@conehealth.com>
2022-01-14 12:08:00 -0500

    fixed for new data stream

--- R/generate_vax_coverage_age.R
+++ R/generate_vax_coverage_age.R
@@ -10,57 +10,68 @@
 
 generate_vax_coverage_age <- function(county_use = nccovid::cone_region){
 
-	vax <- data.table::fread("https://raw.githubusercontent.com/conedatascience/covid-data/master/data/timeseries/nc-summary-all-vaccine.csv")
+	na_to <- function(x, to =0){
+		ifelse(is.na(x),0, x)
+	}
 	
+	vax <- data.table::fread("https://raw.githubusercontent.com/conedatascience/covid-data/master/data/timeseries/nc-summary-all-vaccine.csv")
 	
 	vax <- clean_names(vax)
 	
-	vax_rock <- vax[county%in%county_use][demographic=="Age Group"][data_source=="All Programs"]
+	vax_use <- vax[county%in%county_use][demographic=="Age Group"][data_source=="All Programs"][!age_group%in% c("Suppressed", "Missing or Undisclosed")]
 	
 	
-	full_vax <- vax_rock[!age_group%in% c("Suppressed", "Missing or Undisclosed")]
+	data.table::setorderv(vax_use, cols = c("county", "age_group", "week_of"))
 	
+	vax_use <- vax_use[!is.na(week_of)]
 	
-	full_vax <- full_vax[!is.na(fully_vaccinated_population)& !is.na(week_of)]
-	full_vax[,people_fully_vaccinated:=na_to(people_fully_vaccinated,0)]
+	vax_use[,population:=two_doses_or_one_dose_jj_population]
 	
-	full_vax[order(week_of),cum_vax:=cumsum(people_fully_vaccinated), by = c("county","age_group")]
+	vax_use[,population:=ifelse(is.na(population), max(population, na.rm=TRUE), population), by = c("county", "age_group")]
 	
-	d_latest <- full_vax %>%
-		dplyr::group_by(county) %>%
-		dplyr::filter(week_of == max(week_of, na.rm = TRUE))
+	vax_use[,primary_series_n:=na_to(people_vaccinated_with_two_doses_or_one_dose_jj)]
 	
-	new_titles <- data.table::setDT(d_latest)[,list(Global = scales::percent(sum(cum_vax)/sum(fully_vaccinated_population))), by = "county"]
-	new_titles[,Title_use:=sprintf("%s %s Overall Fully Vaccinated", county, Global)]
-	vax_perc_graph <- data.table::setDT(d_latest)[,list(AgePerc = scales::percent(sum(cum_vax)/sum(fully_vaccinated_population))), by = c("county","age_group")]
-	vax_perc_graph <- merge(vax_perc_graph,new_titles, by = "county" )
+	vax_use[,vax_cum:=cumsum(primary_series_n), by = c("county","age_group")]
 	
-	x <- unique(d_latest$age_group)
+	vax_use[,un_vax_cum:=population - vax_cum , by = c("county","age_group")]
 	
-	y <- stringr::str_extract(x, "\\d\\d|\\d")
+	vax_use[,vax_per := vax_cum/population]
 	
-	x <- x[order(as.numeric(y))]
+	vax_title_bar <- vax_use[week_of==max(week_of)][
+		,.(TotalVaxRate = sum(vax_cum)/sum(population)), by = "county"
+	][,TitleText:=sprintf("%s %s%% Overall Vaccinated",
+												county, round(TotalVaxRate*100))]
 	
-	d_latest %>%
-		dplyr::mutate(age_group = factor(age_group,x)) %>%
-		dplyr::mutate(unvax = fully_vaccinated_population-cum_vax) %>%
-		dplyr::left_join(new_titles, by = "county") %>%
-		dplyr::select(cum_vax,unvax, age_group, Title_use) %>%
-		tidyr::gather(metric, value, -age_group, -Title_use) %>%
-		dplyr::mutate(metric = rev(metric)) %>%
+	vax_perc_graph<- vax_use[week_of==max(week_of)][
+		,.(GroupVaxRate = round(sum(vax_cum)/sum(population)*100)), 
+		by = c("county", "age_group")][,VaxGroupPerc:=sprintf("%s%%", GroupVaxRate)]
+	
+	vax_perc_graph <- merge(vax_perc_graph,vax_title_bar, by = "county", all.x= TRUE)
+	
+	vax_long_version <- vax_use%>%
+		.[week_of==max(week_of)] %>% 
+		dplyr::select(county,age_group, `Unvaccinated` = un_vax_cum, `Vaccinated`= vax_cum) %>% 
+		tidyr::gather(metric, value, -county, -age_group) %>% 
+		dplyr::left_join(vax_title_bar, by = "county") %>% 
+		dplyr::mutate(age_group = factor(age_group, c("0-4", "5-11", "12-17", 
+																					 "18-24", "25-49", "50-64",
+																					 "65-74", "75+")))
+	
+	vax_long_version  %>% 
 		ggplot2::ggplot(ggplot2::aes(age_group, fill = metric, value))+
 		ggplot2::geom_col()+
-		ggplot2::geom_text(data = vax_perc_graph,
-											 ggplot2::aes(x= age_group,y = 100, label = AgePerc), 
-											 inherit.aes=FALSE, hjust=0,
-							color = "grey90", font = "bold", size = 6)+
 		ggplot2::coord_flip()+
+		ggplot2::facet_wrap(~TitleText, scales = "free")+
+		ggplot2::geom_text(data = vax_perc_graph,
+											 ggplot2::aes(x= age_group,y = 100, label = VaxGroupPerc), 
+							inherit.aes=FALSE, hjust=0,
+							color = "grey90", font = "bold", size = 6)+
 		ggplot2::labs(
-			title = "Full Vaccination Coverage",
+			title = "Primary Series Vaccination Coverage",
 			y = "Population",
 			fill = NULL,
 			x = NULL,
-			caption = "Data: NCDHHS"
+			caption = "Data: NCDHHS\nPrimary Series is either 2 Doses of mRNA or 1 Dose of J&J"
 		)+
 		ggplot2::scale_y_continuous(labels = scales::comma)+
 		ggplot2::scale_fill_manual(values =c("grey70", "#00468B"), 
@@ -68,7 +79,6 @@ generate_vax_coverage_age <- function(county_use = nccovid::cone_region){
 		ggplot2::theme_bw()+
 		ggplot2::theme(panel.grid.major.x = ggplot2::element_line(),
 					panel.grid.major.y = ggplot2::element_blank())+
-		ggplot2::facet_wrap(~Title_use, scales = "free")+
 		ggplot2::theme(legend.position = "top")
 	
 }
